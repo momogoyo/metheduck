535563a68885d3248d9f62f7a71db6aff6b5efe4
momogoyo <metheduxk@gmail.com>
2024-07-07 21:41:40 +0900

    🎨 IntersectionObserver로 개선중

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,56 +1,64 @@
 'use client'
 
-import { useEffect, useState, useCallback } from 'react'
+import { useEffect, useState, useCallback, useRef } from 'react'
 import styles from '@/app/ui/gallery/gallery.module.css'
 import Image from 'next/image'
 import { Card, CardContent } from '@/components/ui/card'
 
 export default function Page() {
+  const elementRef = useRef(null)
+
   const [images, setImages] = useState<string[]>([])
   const [page, setPage] = useState(1)
-  const [loading, setLoading] = useState(false)
-  const [hasMore, setHasMore] = useState(false)
+  const [hasMore, setHasMore] = useState(true)
   const PAGESIZE = 20
 
-  const fetchImages = useCallback(async (page: number) => {
-    setLoading(true)
-
+  const fetchImages = async () => {
     try {
+      await new Promise((resolve) => setTimeout(resolve, 500))
+
       const response = await fetch(`/api/images?page=${page}&pageSize=${PAGESIZE}`)
       if (!response.ok) {
         throw new Error(`HTTP error: ${response.status}`)
       }
 
       const data = await response.json()
+
       setImages((prevImages) => [...prevImages, ...data.images])
+      setPage((prevPage) => prevPage + 1)
       setHasMore(page < data.totalPages)
-      setLoading(false)
+      
+      console.log(hasMore, page, images)
     } catch (error) {
       console.error(`Error fetching /api/images: ${error}`)
-      setLoading(false)
+      setHasMore(false)
+    }
+  }
+
+  const onIntersection = (entries: IntersectionObserverEntry[]) => {
+    const entry = entries[0]
+    
+    if (entry.isIntersecting && hasMore) {
+      fetchImages()
+    }
+  }
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(onIntersection)
+
+    if (observer && elementRef.current) {
+      observer.observe(elementRef.current)
+    }
+
+    return () => {
+      if (observer) {
+        observer.disconnect()
+      }
     }
   }, [])
 
-  const handleScroll = useCallback(() => {
-    const scrollHeight = document.documentElement.scrollHeight - 500
-
-    if (window.innerHeight + window.scrollY >= scrollHeight && !loading && hasMore) {
-      setPage((prevPage) => prevPage + 1)
-    }
-  }, [loading, hasMore])
-
-  useEffect(() => {
-    fetchImages(page)
-  }, [page, fetchImages])
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
-
-    return () => window.removeEventListener('scroll', handleScroll)
-  })
-
   return (
-    <div className={styles.wrapper}>
+    <div ref={elementRef} className={styles.wrapper}>
       <div className={styles.gallery}>
         {images.map((src, index) => (
           <Card key={index}>
@@ -65,7 +73,7 @@ export default function Page() {
           </Card>
         ))}
 
-        {loading && <p>Loading..</p>}
+        {hasMore && <p>Loading..</p>}
       </div>
     </div>
   )
